e32e2b1cccc71bbe6653a12fe80b933d3826b11e
Jake Pople <jakepople@hotmail.co.uk>
2025-03-06 23:46:57 +0000

    images.map fix

--- src/components/ShopCards.tsx
+++ src/components/ShopCards.tsx
@@ -5,9 +5,14 @@ import { useEffect, useState } from "react";
 interface ImageData {
   public_id: string;
   image_url: string;
-  metadata: { caption?: string; alt?: string; link_to?: string; online?: string; };
+  metadata: {
+    caption?: string;
+    alt?: string;
+    link_to?: string;
+    online?: string;
+  };
   tags: string[];
-};
+}
 
 // type GroupedImages = {
 //   images: Record<string, ImageData[]>;
@@ -16,7 +21,7 @@ interface ImageData {
 const ShopCards = () => {
   const [images, setImages] = useState<ImageData[]>([]);
 
-  console.log(images)
+  console.log(images);
 
   useEffect(() => {
     const tag = "shop";
@@ -24,21 +29,6 @@ const ShopCards = () => {
       .get(`${import.meta.env.VITE_BACKEND_URL}api/images/by-tag?tag=${tag}`)
       .then((res) => {
         console.log(res.data.images);
-        //   [
-        //     {
-        //         "public_id": "samples/coffee",
-        //         "image_url": "https://res.cloudinary.com/dcrdxjfgg/image/upload/v1739880804/samples/coffee.jpg",
-        //         "metadata": {
-        //             "alt": "Coffee shop image.",
-        //             "caption": "Lowther Coffee Shop Test",
-        //             "link_to": "https://www.lowther.co.uk/",
-        //             "online": "false"
-        //         },
-        //         "tags": [
-        //             "shop"
-        //         ]
-        //     }
-        // ]
         setImages(res.data.images);
       })
       .catch((err) => console.error("Error fetching images:", err));
@@ -56,17 +46,18 @@ const ShopCards = () => {
       />
       <div className="flex flex-wrap gap-10 mt-10">
         {images.map((image) => (
-          <>
-            <ShopCard
-              key={image.public_id}
-              className="w-full md:w-[400px]"
-              title={image.metadata.caption || ""}
-              onlineShop={image.metadata.online == "true"}
-              linkTo={image.metadata.link_to || "#"}
-              imgSrc={image.image_url || "https://res.cloudinary.com/dcrdxjfgg/image/upload/v1741087977/Beaver_reredux_fmk9gd.png"} // Default image
-              imgAlt={image.metadata.alt || ""}
-            />
-          </>
+          <ShopCard
+            key={image.public_id}
+            className="w-full md:w-[400px]"
+            title={image.metadata.caption || ""}
+            onlineShop={image.metadata.online == "true"}
+            linkTo={image.metadata.link_to || "#"}
+            imgSrc={
+              image.image_url ||
+              "https://res.cloudinary.com/dcrdxjfgg/image/upload/v1741087977/Beaver_reredux_fmk9gd.png"
+            } // Default image
+            imgAlt={image.metadata.alt || ""}
+          />
         ))}
       </div>
     </div>
